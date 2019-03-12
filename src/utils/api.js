36ccdd8bf4265a6470e5ce892c29b597eb4a5ceb
Martin Tudor <martcan27@gmail.com>
2019-03-12 16:21:35 +0000

    error handling

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,12 @@ import memoize from "memoizee";
 
 const APIkey = "3d7abca039d3fb9e0340ec05e86f1c55";
 
+function handleError(error) {
+    // eslint-disable-next-line no-console
+    console.warn(error);
+    return null;
+}
+
 function fetchForecast(city) {
     const url = window.encodeURI(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIkey}`
@@ -18,12 +24,15 @@ function fetchFiveDay(city) {
         `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${APIkey}`
     );
     console.log(url);
-    return axios.get(url).then(function(response) {
-        return response.data;
-    });
+    return axios
+        .get(url)
+        .then(function(response) {
+            return response.data;
+        })
+        .catch(handleError);
 }
 
-const memoized = memoize(fetchFiveDay);
+const memoized = memoize(fetchFiveDay, { maxAge: 600000 });
 
 export default {
     fetchForecast,
