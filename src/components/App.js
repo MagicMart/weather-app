cd11696504193157bbff6cb331bc4ea76fc7cd47
Martin Tudor <martcan27@gmail.com>
2019-05-01 20:56:37 +0100

    code-splitting

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import loadable from "@loadable/component";
 import Header from "./Header";
 import Home from "./Home";
-import Forecast from "./Forecast";
-import Details from "./Details";
+
+const Forecast = loadable(() => import("./Forecast"), {
+    fallback: <h2>Loading</h2>
+});
+const Details = loadable(() => import("./Details"), {
+    fallback: <h2>Loading</h2>
+});
+
+// import Details from "./Details";
 
 function App() {
     return (
@@ -12,8 +20,14 @@ function App() {
                 <Header />
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/forecast" component={Forecast} />
-                    <Route path="/details" component={Details} />
+                    <Route
+                        path="/forecast"
+                        render={props => <Forecast {...props} />}
+                    />
+                    <Route
+                        path="/details"
+                        render={props => <Details {...props} />}
+                    />
                     <Route render={() => <p>Not Found</p>} />
                 </Switch>
             </div>
