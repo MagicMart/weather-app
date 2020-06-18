ff3e97bce8f684349ecc5865b41f3902c9eb7ade
Martin Tudor <martcan27@gmail.com>
2020-06-18 14:17:06 +0100

    Revert "use apigateway"
    This reverts commit aff461f3a8e5f7eaf9196dafd38a9be0a0a4828e.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import memoize from "memoizee";
+import { API_KEY } from "./key";
 
 function handleError(error) {
     // eslint-disable-next-line no-console
@@ -7,10 +8,30 @@ function handleError(error) {
     return error.response;
 }
 
+// function fetchForecast(city) {
+//     const url = window.encodeURI(
+//         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`
+//     );
+//     return axios.get(url).then(function(response) {
+//         return response.data;
+//     });
+// }
+
+// API call:
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={your api key}
+// Parameters:
+// lat, lon coordinates of the location of your interest
+// Examples of API calls:
+// api.openweathermap.org/data/2.5/forecast?lat=35&lon=139
+
+const api = "https://api.openweathermap.org/data/2.5";
+
 const fetchForecast = memoize(
     function FiveDay(loc) {
-        const { lat, lng } = loc;
-        const url = `https://vh7fs6xb15.execute-api.eu-west-2.amazonaws.com/fetchforcast?lat=${lat}&lng=${lng}`;
+        const url = window.encodeURI(
+            `${api}/forecast?lat=${loc.lat}&lon=${loc.lng}&appid=${API_KEY}`
+        );
+
         return axios
             .get(url)
             .then(function (response) {
