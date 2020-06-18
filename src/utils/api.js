1f989e9e4d5e949fae1fb48515b970e9b01e4424
Martin Tudor <martcan27@gmail.com>
2020-06-18 14:22:06 +0100

    use fetch

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import memoize from "memoizee";
 import { API_KEY } from "./key";
 
@@ -8,35 +7,15 @@ function handleError(error) {
     return error.response;
 }
 
-// function fetchForecast(city) {
-//     const url = window.encodeURI(
-//         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`
-//     );
-//     return axios.get(url).then(function(response) {
-//         return response.data;
-//     });
-// }
-
-// API call:
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={your api key}
-// Parameters:
-// lat, lon coordinates of the location of your interest
-// Examples of API calls:
-// api.openweathermap.org/data/2.5/forecast?lat=35&lon=139
-
 const api = "https://api.openweathermap.org/data/2.5";
 
 const fetchForecast = memoize(
     function FiveDay(loc) {
-        const url = window.encodeURI(
-            `${api}/forecast?lat=${loc.lat}&lon=${loc.lng}&appid=${API_KEY}`
-        );
+        const url = `${api}/forecast?lat=${loc.lat}&lon=${loc.lng}&appid=${API_KEY}`;
 
-        return axios
-            .get(url)
-            .then(function (response) {
-                return response.data;
-            })
+        return fetch(url)
+            .then((res) => res.json)
+            .then((data) => data)
             .catch(handleError);
     },
     { maxAge: 600000 }
