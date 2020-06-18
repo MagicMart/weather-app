4915444e48fcc1080ff2ae98a16b989a19dedf97
Martin Tudor <martcan27@gmail.com>
2020-06-18 17:31:54 +0100

    tidy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,4 @@
 import memoize from "memoizee";
-// import { API_KEY } from "./key";
 
 function handleError(error) {
     // eslint-disable-next-line no-console
@@ -7,12 +6,12 @@ function handleError(error) {
     return error.response;
 }
 
-const api =
+const endpoint =
     "https://vh7fs6xb15.execute-api.eu-west-2.amazonaws.com/fetchforecast?";
 
 const fetchForecast = memoize(
     function FiveDay(loc) {
-        const url = `${api}lat=${loc.lat}&lng=${loc.lng}`;
+        const url = `${endpoint}lat=${loc.lat}&lng=${loc.lng}`;
 
         return fetch(url)
             .then((res) => res.json())
